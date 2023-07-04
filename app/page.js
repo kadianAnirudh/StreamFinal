1dd1283e556b55dd5aa56c83d5ee1d5989c7b999
unknown <coderanirudh@gmail.com>
2023-07-04 13:19:25 +0530

    changed a link on homepage

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             AI assistant. Please Login to continue
           </p>
           <Link
-            href="https://github.com/kadianAnirudh/Stream"
+            href="https://github.com/kadianAnirudh/StreamFinal"
             className="inline-flex items-center justify-center px-5 py-3 mx-4 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             Github
